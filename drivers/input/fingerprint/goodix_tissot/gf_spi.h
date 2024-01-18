1eb268982d5ec8192c01c3daa6427488eb2cf77b
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2024-01-18 14:56:05 +0000

    drivers: Adapt goodix FP from 4.9 to 4.19

--- drivers/input/fingerprint/goodix_tissot/gf_spi.h
+++ drivers/input/fingerprint/goodix_tissot/gf_spi.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 #include <linux/notifier.h>
 /**********************************************************/
-enum FP_MODE {
+enum FP_MODE{
 	GF_IMAGE_MODE = 0,
 	GF_KEY_MODE,
 	GF_SLEEP_MODE,
@@ -13,13 +13,23 @@ enum FP_MODE {
 };
 
 #define SUPPORT_NAV_EVENT
-#if defined(SUPPORT_NAV_EVENT)
 
+#if defined(SUPPORT_NAV_EVENT)
+#define GF_NAV_INPUT_UP			KEY_UP
+#define GF_NAV_INPUT_DOWN		KEY_DOWN
+#define GF_NAV_INPUT_LEFT		KEY_LEFT
+#define GF_NAV_INPUT_RIGHT		KEY_RIGHT
+#define GF_NAV_INPUT_CLICK		KEY_HOME
 #define GF_NAV_INPUT_DOUBLE_CLICK	KEY_VOLUMEUP
 #define GF_NAV_INPUT_LONG_PRESS		KEY_SEARCH
 #define GF_NAV_INPUT_HEAVY		KEY_CHAT
 #endif
 
+#define GF_KEY_INPUT_HOME		KEY_HOMEPAGE
+#define GF_KEY_INPUT_MENU		KEY_MENU
+#define GF_KEY_INPUT_BACK		KEY_BACK
+#define GF_KEY_INPUT_POWER		KEY_POWER
+#define GF_KEY_INPUT_CAMERA		KEY_CAMERA
 
 #if defined(SUPPORT_NAV_EVENT)
 typedef enum gf_nav_event {
@@ -43,25 +53,18 @@ typedef enum gf_key_event {
 	GF_KEY_POWER,
 	GF_KEY_MENU,
 	GF_KEY_BACK,
-	GF_KEY_CAPTURE,
-	GF_KEY_UP,
-	GF_KEY_DOWN,
+	GF_KEY_CAMERA,
 	GF_KEY_RIGHT,
-	GF_KEY_LEFT,
-	GF_KEY_TAP,
-	GF_KEY_HEAVY,
-	GF_KEY_LONG_PRESS,
-	GF_KEY_DOUBLE_TAP
 } gf_key_event_t;
 
 struct gf_key {
 	enum gf_key_event key;
-	uint32_t value; /* key down = 1, key up = 0 */
+	uint32_t value;   /* key down = 1, key up = 0 */
 };
 
 struct gf_key_map {
-	char *name;
-	unsigned short val;
+	unsigned int type;
+	unsigned int code;
 };
 
 struct gf_ioc_chip_info {
@@ -71,7 +74,7 @@ struct gf_ioc_chip_info {
 	unsigned char reserved[5];
 };
 
-#define GF_IOC_MAGIC    'g'
+#define GF_IOC_MAGIC    'g'     /*define magic number*/
 #define GF_IOC_INIT             _IOR(GF_IOC_MAGIC, 0, uint8_t)
 #define GF_IOC_EXIT             _IO(GF_IOC_MAGIC, 1)
 #define GF_IOC_RESET            _IO(GF_IOC_MAGIC, 2)
@@ -85,9 +88,7 @@ struct gf_ioc_chip_info {
 #define GF_IOC_ENTER_SLEEP_MODE _IO(GF_IOC_MAGIC, 10)
 #define GF_IOC_GET_FW_INFO      _IOR(GF_IOC_MAGIC, 11, uint8_t)
 #define GF_IOC_REMOVE           _IO(GF_IOC_MAGIC, 12)
-#define GF_IOC_CHIP_INFO        _IOR(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
-#define GF_IOC_ENABLE_GPIO  	_IO(GF_IOC_MAGIC, 15)
-#define GF_IOC_RELEASE_GPIO  	_IO(GF_IOC_MAGIC, 16)
+#define GF_IOC_CHIP_INFO        _IOW(GF_IOC_MAGIC, 13, struct gf_ioc_chip_info)
 
 #if defined(SUPPORT_NAV_EVENT)
 #define GF_IOC_NAV_EVENT	_IOW(GF_IOC_MAGIC, 14, gf_nav_event_t)
@@ -96,25 +97,15 @@ struct gf_ioc_chip_info {
 #define  GF_IOC_MAXNR    14  /* THIS MACRO IS NOT USED NOW... */
 #endif
 
-
+/*#define AP_CONTROL_CLK       1*/
 #define  USE_PLATFORM_BUS     1
-
-
+/*#define  USE_SPI_BUS	1*/
+/*#define GF_FASYNC   1*/ /*If support fasync mechanism.*/
 #define GF_NETLINK_ENABLE 1
 #define GF_NET_EVENT_IRQ 1
 #define GF_NET_EVENT_FB_BLACK 2
 #define GF_NET_EVENT_FB_UNBLACK 3
 #define NETLINK_TEST 25
-
-
-#ifdef ENABLE_PINCTRL
-static const char * const pctl_names[] = {
-
-	"goodixfp_reset_reset",
-	"goodixfp_reset_active",
-	"goodixfp_irq_active",
-};
-#endif
 
 struct gf_dev {
 	dev_t devt;
@@ -126,11 +117,6 @@ struct gf_dev {
 #endif
 	struct clk *core_clk;
 	struct clk *iface_clk;
-
-#ifdef ENABLE_PINCTRL
-	struct pinctrl *fingerprint_pinctrl;
-	struct pinctrl_state *pinctrl_state[ARRAY_SIZE(pctl_names)];
-#endif
 
 	struct input_dev *input;
 	/* buffer is NULL unless this device is open (users > 0) */
@@ -149,7 +135,6 @@ struct gf_dev {
 	char fb_black;
 	char wait_finger_down;
 	struct work_struct work;
-
 };
 
 int gf_parse_dts(struct gf_dev *gf_dev);
