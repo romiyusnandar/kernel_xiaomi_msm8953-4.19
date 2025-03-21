49807019f22fadd5c6a18e640b7642c37a343320
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-03-21 01:54:18 +0000

    Revert "camera_v2: Fixup Front Flash"
    This reverts commit 30cb03123b5ac451e40b516aae2fa75dfd609784.

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.c
@@ -85,7 +85,7 @@ static struct led_classdev msm_torch_led[MAX_LED_TRIGGERS] = {
 		.brightness	= LED_OFF,
 	},
 };
-static int msm_torch_led_num;
+
 static int32_t msm_torch_create_classdev(struct platform_device *pdev,
 				void *data)
 {
@@ -104,17 +104,16 @@ static int32_t msm_torch_create_classdev(struct platform_device *pdev,
 			torch_trigger = fctrl->torch_trigger[i];
 			CDBG("%s:%d msm_torch_brightness_set for torch %d",
 				__func__, __LINE__, i);
-			msm_torch_brightness_set(&msm_torch_led[msm_torch_led_num + i],
+			msm_torch_brightness_set(&msm_torch_led[i],
 				LED_OFF);
 
 			rc = led_classdev_register(&pdev->dev,
-				&msm_torch_led[msm_torch_led_num + i]);
+				&msm_torch_led[i]);
 			if (rc) {
 				pr_err("Failed to register %d led dev. rc = %d\n",
 						i, rc);
 				return rc;
 			}
-			msm_torch_led_num++;
 		} else {
 			pr_err("Invalid fctrl->torch_trigger[%d]\n", i);
 			return -EINVAL;
@@ -640,7 +639,9 @@ static int32_t msm_flash_low(
 				pr_debug("LED current clamped to %d\n",
 					curr);
 			}
-			led_trigger_event(flash_ctrl->torch_trigger[i], curr);
+			CDBG("low_flash_current[%d] = %d", i, curr);
+			led_trigger_event(flash_ctrl->torch_trigger[i],
+				curr);
 		}
 	}
 	if (flash_ctrl->switch_trigger)
@@ -731,6 +732,8 @@ static int32_t msm_flash_config(struct msm_flash_ctrl_t *flash_ctrl,
 		(struct msm_flash_cfg_data_t *) argp;
 
 	mutex_lock(flash_ctrl->flash_mutex);
+
+	CDBG("Enter %s type %d\n", __func__, flash_data->cfg_type);
 
 	switch (flash_data->cfg_type) {
 	case CFG_FLASH_INIT:
