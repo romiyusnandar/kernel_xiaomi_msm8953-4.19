e8a03cd00d07cfe3deb0835d85d3dd5d0a7f99ee
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-01 05:34:13 +0000

    techpack: stub: Define gesture_incall
    * Custom DT2W/S2W relies on this

--- techpack/stub/stub.c
+++ techpack/stub/stub.c
@@ -2,11 +2,7 @@
 
 #include <linux/types.h>
 
-#if IS_ENABLED(CONFIG_TECHPACK_AUDIO_DISABLE)
-#if (defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) || defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE))
 bool gesture_incall = false;
-#endif
-#endif
 
 static void _techpack_stub(void)
 {
